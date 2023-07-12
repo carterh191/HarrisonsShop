03b2af6ad38b1a28514218808fc8d8c7a7358040
Harrison <38048257+carterh191@users.noreply.github.com>
2023-07-11 19:54:19 -0700

    created CategoryRepository
    implements ICategoryRepository interface. using contructor injection

---
+++ HarrisonsShop/Models/CategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace HarrisonsShop.Models
+
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly HarrisonsShopDbContext _harrisonsPieShopDbContext;
+
+        public CategoryRepository(HarrionsShopDbContext harrisonsPieShopDbContext)
+        {
+            _harrisonsPieShopDbContext = harrisonsPieShopDbContext;
+        }
+
+        public IEnumerable<Category> AllCategories => _harrisonsPieShopDbContext.Categories.OrderBy(p => p.CategoryName);
+    }
+}

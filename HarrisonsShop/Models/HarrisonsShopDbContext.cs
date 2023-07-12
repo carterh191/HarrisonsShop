9496416213554f6124833bcf9410845f5b41c695
Harrison <38048257+carterh191@users.noreply.github.com>
2023-07-11 19:21:55 -0700

    Appsetting
    updated connectionStrings for appsettings mock local db

--- HarrisonsShop/Models/HarrisonsShopDbContext.cs
+++ HarrisonsShop/Models/HarrisonsShopDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace HarrisonsShop.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HarrisonsShop.Models
 {
-    public class HarrisonsShopDbContext
+    public class HarrisonsShopDbContext : DbContext
     {
+        public HarrisonsShopDbContext(DbContextOptions<HarrisonsShopDbContext> options) : base(options) //contructor for db context options for thing to work correctly
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; } //set to EF core entities to match
+        public DbSet<Pie> Pies { get; set; } //set to EF core entities to match
     }
 }
